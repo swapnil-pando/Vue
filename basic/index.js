d270209e8d89e4fcdeb1407a2b41f3bd327edeff
Swapnil Satpathy <swapnilsatpathy@pando.ai>
2021-12-22 13:19:29 +0530

    Add code for Event Handling

--- basic/index.js
+++ basic/index.js
@@ -1,13 +1,28 @@
 const app = Vue.createApp({
     data(){
         return {
+            cart:0,
             product:"Socks",
             desc:"This is about Socks",
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             onSale: false,
             details: ['50% cotton','30% wool'],
-            sizes: ['XS','S','M','L','XL']
+            sizes: ['XS','S','M','L','XL'],
+            showDelButton: false
+        }
+    },
+    methods:{
+        addToCart(){
+            
+            this.cart+=1
+            if(this.cart > 0)
+                this.showDelButton=true
+        },
+        removeFromCart(){
+            this.cart-=1
+            if(this.cart === 0)
+                this.showDelbutton = false
         }
     }
 })
